9968afd5802755b9c315889b2d2494588c41e682
Pavel <bondarovich@kinetica.su>
2014-05-15 13:08:30 +0700

    +filename attach

--- lib/core/sfPhastMailing.class.php
+++ lib/core/sfPhastMailing.class.php
@@ -18,6 +18,7 @@ class sfPhastMailing{
         if(($transport = $mailer->getTransport()) instanceOf Swift_MailTransport){
 			$transport->setExtraParams(null);
 		}
+		$mail = $mailer->compose($from, $recipient, $subject, $body);
 		if(isset($parameters['attach']) && is_array($parameters['attach'])){
             foreach($parameters['attach'] as $key => $value){
                 $mail->attach(
@@ -27,7 +28,6 @@ class sfPhastMailing{
 				);
             }
         }
-        $mail = $mailer->compose($from, $recipient, $subject, $body);
         return $mailer->send($mail);
     }
 
