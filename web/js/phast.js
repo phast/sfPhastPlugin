ee4c69d1b31bbb370f8efffa30c7069f4fada88b
Pavel B <mrpro@mail.ru>
2014-09-10 11:36:03 +0700

    tinimce settings

--- web/js/phast.js
+++ web/js/phast.js
@@ -1401,7 +1401,7 @@ $.fn.serializeJSON = function(){
             force_br_newlines : false,
             force_p_newlines : true,
 
-            content_css : "/css/tinymce.css?3"
+            content_css : "/css/tinymce.css"
         },
 
         edit: {
@@ -1433,7 +1433,7 @@ $.fn.serializeJSON = function(){
             force_br_newlines : false,
             force_p_newlines : true,
 
-            content_css : "/css/edit_tinymce.css?3"
+            content_css : "/css/tinymce.css"
         },
 
         link: {
@@ -1462,7 +1462,7 @@ $.fn.serializeJSON = function(){
             force_br_newlines : false,
             force_p_newlines : true,
 
-            content_css : "/css/tinymce.css?3"
+            content_css : "/css/tinymce.css"
 
         }
 	};
@@ -1490,67 +1490,3 @@ $.fn.serializeJSON = function(){
 
 })(window);
 
-/*
-	script_url : '/js/lib/tinymce/tiny_mce.js',
-
-	// General options
-	theme : "advanced",
-	plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
-
-	// Theme options
-	theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "bottom",
-	theme_advanced_resizing : true,
-
-	// Example content CSS (should be your site CSS)
-	content_css : "/css/content.css",
-
-	// Drop lists for link/image/media/template dialogs
-	template_external_list_url : "lists/template_list.js",
-	external_link_list_url : "lists/link_list.js",
-	external_image_list_url : "lists/image_list.js",
-	media_external_list_url : "lists/media_list.js",
-
-	// Replace values for the template plugin
-	template_replace_values : {
-	username : "Some User",
-		staffid : "991234"
-}
-*/
-
-
-
-/*
-tinyMCE.init({
-	mode : "specific_textareas",
-	editor_selector : "phast-box-textedit",
-	language : "ru",
-	theme : "advanced",
-	element_format : "html",
-	plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-	dialog_type : "modal",
-	file_browser_callback : "Filebrowser.open",
-
-	font_size_classes : "h",
-	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,forecolor,backcolor,|,fontsizeselect,formatselect,|,cite,abbr,acronym,del,ins,|,styleprops,|,attribs,|,visualchars,nonbreaking,template,pagebreak",
-	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,preview",
-	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "none",
-	theme_advanced_resizing : false,
-
-	fix_list_elements : true,
-	fix_table_elements : true,
-	fix_nesting : true,
-	cleanup_on_startup : true,
-	convert_urls : false,
-
-	content_css : "/css/system/tinymce.css?1"
-});
-*/
