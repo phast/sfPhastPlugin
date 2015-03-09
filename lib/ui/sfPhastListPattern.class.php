4413bec74b70682239a9d272bc1f22bfae206b68
Pavel B <p.bondarovich@gmail.com>
2015-03-09 09:37:42 +0600

    * short addControl for listPattern

--- lib/ui/sfPhastListPattern.class.php
+++ lib/ui/sfPhastListPattern.class.php
@@ -292,6 +292,16 @@ class sfPhastListPattern
 
 	public function addControl($options)
 	{
+        if(!is_array($options)){
+            $args = func_get_args();
+            $options = [
+                'caption' => $args[0],
+                'icon' => $args[1],
+                'action' => $args[2],
+            ];
+
+        }
+
 		if(isset($options['action'])){
 			$options['action'] = sfPhastUI::parseScript($options['action'], array('model' => array('list: list'), 'parameters' => array('relation: item.$pk')));
 		}
